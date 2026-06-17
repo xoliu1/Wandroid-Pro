22cb97064df657cffc5743b8839a831fa5351691
xoliu <xoliu@example.com>
2026-06-17 18:31:48 +0800

    ci: remove codegen dependency from release build

--- lib/model/login/User.dart
+++ lib/model/login/User.dart
@@ -1,10 +1,4 @@
 
-
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'User.g.dart';
-
-@JsonSerializable()
 class User {
   List<String> chapterTops;
   List<int> collectIds;
@@ -19,8 +13,31 @@ class User {
   User(this.chapterTops, this.collectIds, this.email, this.icon,
       this.id, this.password, this.token, this.type, this.username);
 
-  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        (json['chapterTops'] as List<dynamic>? ?? const [])
+            .map((e) => e as String)
+            .toList(),
+        (json['collectIds'] as List<dynamic>? ?? const [])
+            .map((e) => (e as num).toInt())
+            .toList(),
+        json['email'] as String? ?? '',
+        json['icon'] as String? ?? '',
+        (json['id'] as num?)?.toInt() ?? 0,
+        json['password'] as String? ?? '',
+        json['token'] as String? ?? '',
+        (json['type'] as num?)?.toInt() ?? 0,
+        json['username'] as String? ?? '',
+      );
 
-  Map<String, dynamic> toJson() => _$UserToJson(this);
-
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'chapterTops': chapterTops,
+        'collectIds': collectIds,
+        'email': email,
+        'icon': icon,
+        'id': id,
+        'password': password,
+        'token': token,
+        'type': type,
+        'username': username,
+      };
 }
